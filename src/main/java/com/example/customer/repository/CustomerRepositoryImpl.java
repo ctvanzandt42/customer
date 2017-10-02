51a42b468267f7af4a544adccc6a6c467ec642fb
ctvanzandt42 <ctvanzandt42@gmail.com>
2017-10-02 16:49:06 -0500

    customer impl done

---
+++ src/main/java/com/example/customer/repository/CustomerRepositoryImpl.java
@@ -0,0 +1,69 @@
+package com.example.customer.repository;
+
+import com.example.customer.model.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+@Repository
+public class CustomerRepositoryImpl implements CustomerRepository{
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    private String ADD_SQL = "INSERT INTO customer (firstName, lastName, phone, email) VALUES (?,?,?,?)";
+
+    @Override
+    public void add(Customer customer) {
+        jdbcTemplate.update(ADD_SQL, customer.getFirstName(), customer.getLastName(), customer.getPhone(),
+                customer.getEmail());
+    }
+
+    private String UPDATE_SQL = "UPDATE customer SET firstName = ?, lastName = ?, phone = ?, email = ? WHERE id = ?";
+
+    @Override
+    public void update(Customer customer) {
+        jdbcTemplate.update(UPDATE_SQL, customer.getFirstName(), customer.getLastName(), customer.getPhone(),
+                customer.getEmail(), customer.getId());
+    }
+
+    private String GET_BY_ID_SQL = "SELECT * FROM customer WHERE id = ?";
+
+    @Override
+    public Customer get(int id) {
+        return jdbcTemplate.queryForObject(GET_BY_ID_SQL, new CustomerMapper(), id);
+    }
+
+    private String GET_ALL_SQL = "SELECT * FROM customer";
+
+    @Override
+    public List<Customer> get() {
+        return jdbcTemplate.query(GET_ALL_SQL, new CustomerMapper());
+    }
+
+    private String DELETE_SQL = "DELETE FROM customer WHERE id = ?";
+
+    @Override
+    public void delete(Customer customer) {
+        jdbcTemplate.update(DELETE_SQL, customer.getFirstName(), customer.getLastName(), customer.getPhone(),
+                customer.getEmail(), customer.getId());
+    }
+
+    private class CustomerMapper implements RowMapper<Customer> {
+        @Override
+        public Customer mapRow(ResultSet resultSet, int i) throws SQLException {
+            Customer customer = new Customer();
+            customer.setId(resultSet.getInt("id"));
+            customer.setFirstName(resultSet.getString("firstName"));
+            customer.setLastName(resultSet.getString("lastName"));
+            customer.setPhone(resultSet.getString("phone"));
+            customer.setEmail(resultSet.getString("email"));
+            return customer;
+        }
+    }
+}
