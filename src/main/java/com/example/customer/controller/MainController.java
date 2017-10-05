53fcf848a1045bbaa00af157b57a471287644b77
ctvanzandt42 <ctvanzandt42@gmail.com>
2017-10-05 08:14:18 -0500

    routes working

--- src/main/java/com/example/customer/controller/MainController.java
+++ src/main/java/com/example/customer/controller/MainController.java
@@ -2,10 +2,7 @@ package com.example.customer.controller;
 
 
 import com.example.customer.model.Customer;
-import com.example.customer.repository.CustomerRepository;
-import com.example.customer.repository.CustomerRepositoryImpl;
 import com.example.customer.service.CustomerService;
-import com.example.customer.service.CustomerServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
